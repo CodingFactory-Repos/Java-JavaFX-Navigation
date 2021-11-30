581c0570fb16e78524acd90e19e98397e87e778b
Louis <35641311+Loule95450@users.noreply.github.com>
2021-11-30 13:24:49 +0100

    🪑 - Add Fauteuil and Commode

---
+++ src/main/java/me/loule/navigation/controller/CommodeController.java
@@ -0,0 +1,18 @@
+package me.loule.navigation.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class CommodeController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
+    
+}
--- src/main/java/me/loule/navigation/controller/MainController.java
+++ src/main/java/me/loule/navigation/controller/MainController.java
@@ -1,14 +1,47 @@
 package me.loule.navigation.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
-public class MainController {
+public class MainController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private VBox frameCommode;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private VBox frameFauteuil;
+
+    @FXML
+    private AnchorPane pageCentre;
+
+    @FXML
+    private Button btnCommode;
+
+    @FXML
+    private Button btnFauteuil;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        clearPageCentre();
+
+        btnCommode.setOnMouseClicked(btnCommodeAction -> {
+            clearPageCentre();
+            pageCentre.getChildren().addAll(frameCommode);
+        });
+
+        btnFauteuil.setOnMouseClicked(btnFauteuilAction -> {
+            clearPageCentre();
+            pageCentre.getChildren().addAll(frameFauteuil);
+        });
+    }
+
+    public void clearPageCentre() {
+        pageCentre.getChildren().removeAll(frameCommode);
+        pageCentre.getChildren().removeAll(frameFauteuil);
     }
 }
